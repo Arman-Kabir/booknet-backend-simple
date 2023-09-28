2c02299466ad4633b78ecf0f62cfa7b562daf725
Arman-Kabir <armankabir8@gmail.com>
2023-09-29 01:12:35 +0600

    api modification

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId, BSON } = require('mongodb');
 const app = express()
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 const port = process.env.PORT
-const { MongoClient, ServerApiVersion } = require('mongodb');
 // -------------------------------------
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.vst2gce.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -16,18 +16,43 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+client.connect();
 
 async function run() {
   try {
-    await client.connect();
-    const booksCollection =  client.db("booknet-simple").collection('books');
-    console.log("successfully connected to MongoDB!");
 
-    
+    const db = client.db('booknet-simple');
+    const booksCollection = db.collection('books');
+
+    app.get('/books', async (req, res) => {
+      const result = await booksCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    app.get('/books/:id', async (req, res) => {
+      const { id } = req.params;
+      // console.log(id);
+
+      const result = await booksCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    app.post('/books', async (req, res) => {
+      const data = req.body;
+      const result = await booksCollection.insertOne(data);
+      res.send({ result });
+    });
+
+    app.post('/books/:id', async (req, res) => {
+      const data = req.body;
+      const { id } = req.params;
+      const result = await booksCollection.updateOne({ _id: new ObjectId(id) }, { $set: data });
+      res.send({ result });
+    });
 
   } finally {
-   
-    await client.close();
+
+
   }
 }
 run().catch(console.dir);
